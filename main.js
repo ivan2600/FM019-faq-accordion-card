d0872877057b9fffbc47fec708b0a33f764f473f
Ivan Adamo <ivan.adamo.8@gmail.com>
2024-03-02 20:34:09 -0300

    lalala

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('funciona');
+const question = document.querySelectorAll('.question');
+const block = document.querySelectorAll('.block');
+
+question.forEach( (item , i) => {
+  question[i].addEventListener('click', ()=>{
+
+    block.forEach(( cadaQuestion, i)=>{
+      block[i].classList.remove('active');
+    })
+
+    block[i].classList.add('active');
+    // if (question[i].classList.contains('active')) {
+    //   console.log(1);
+    //   question[i].classList.remove('active')
+    // } else {
+    //   console.log(2);
+    //   question[i].classList.add('active')
+    // }
+  })
+})
